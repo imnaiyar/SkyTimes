8e02d48a93db275228a06cb713526e2ce10cec83
imnaiyar <137700126+imnaiyar@users.noreply.github.com.>
2026-07-14 13:55:40 +0600

    Add centralized app configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,23 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
 }
+
+tasks.register("syncIosConfig") {
+    group = "ios"
+    description = "Synchronizes app configuration to iosApp/Configuration/Config.xcconfig"
+
+    val appId = project.findProperty("app.id")
+    val appName = project.findProperty("app.name")
+    val configFile = file("iosApp/Configuration/Config.xcconfig")
+
+    doLast {
+        val content = configFile.readLines().joinToString("\n") { line ->
+            when {
+                line.startsWith("APP_ID=") -> "APP_ID=$appId"
+                line.startsWith("APP_NAME=") -> "APP_NAME=$appName"
+                else -> line
+            }
+        }
+        configFile.writeText(content + "\n")
+    }
+}
--- webApp/build.gradle.kts
+++ webApp/build.gradle.kts
@@ -26,3 +26,27 @@ kotlin {
         }
     }
 }
+
+val syncWebTitle by tasks.registering {
+    description = "Syncs app configuration like name  with index.html"
+
+    val appName = project.findProperty("app.name").toString()
+    val resourcesDir = file("src/webMain/resources")
+    val indexHtml = resourcesDir.resolve("index.html")
+
+    doLast {
+        if (indexHtml.exists()) {
+            val content = indexHtml.readText()
+            val newContent = content.replace(Regex("<title>.*</title>"), "<title>$appName</title>")
+            indexHtml.writeText(newContent)
+        }
+    }
+}
+
+tasks.named("jsProcessResources") {
+    dependsOn(syncWebTitle)
+}
+
+tasks.named("wasmJsProcessResources") {
+    dependsOn(syncWebTitle)
+}
